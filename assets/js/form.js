11eb9f3889c413c09e1a1764037ab6756a39d7d0
Criste Phillips <criste.phillips@yahoo.com>
2025-03-04 15:33:45 -0500

    style and text changes, js adjustments to form

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,22 +3,27 @@ form.addEventListener("submit", function (event) {
   event.preventDefault(); // prevents form from autosubmitting
 
   let characters = document.getElementById("characters").value;
-
   let numbersYes = document.getElementById("numbers-yes").value;
-
   let numbersNo = document.getElementById("numbers-no").value;
-
   let upperYes = document.getElementById("upper-yes").value;
-
   let upperNo = document.getElementById("upper-no").value;
-
   let lowerYes = document.getElementById("lower-yes").value;
-
-  let owerNo = document.getElementById("lower-no").value;
-
+  let lowerNo = document.getElementById("lower-no").value;
   let symbolsYes = document.getElementById("symbols-yes").value;
-
   let symbolsNo = document.getElementById("symbols-no").value;
+  let errorMsg = document.querySelector("#errorMsg");
 
-  console.log(symbolsNo);
-});
+  // js for form:
+  // numbers 6-225
+//   if (characters.value.length === 0 || isNaN(characters.value)) {
+//     errorMsg.innerHTML = "Please enter a number between 6 and 255";   
+//   } else 
+//  });
+
+  // checkbox for numbers-yes & numbers-no
+
+  
+  // checkbox for upper-yes and upper-no
+  // checkbox for lower Yes & lower no
+  // checkbox for symbols yes & symbols
+}); 
