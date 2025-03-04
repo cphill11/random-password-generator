f86af58d6f073afe54469060c83119397050007e
Criste Phillips <criste.phillips@yahoo.com>
2025-03-04 18:26:31 -0500

    update to forms

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,19 @@
-const form = document.getElementById("password-form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // prevents form from autosubmitting
 
-  let characters = document.getElementById("characters").value;
-  let numbersYes = document.getElementById("numbers-yes").value;
-  let numbersNo = document.getElementById("numbers-no").value;
-  let upperYes = document.getElementById("upper-yes").value;
-  let upperNo = document.getElementById("upper-no").value;
-  let lowerYes = document.getElementById("lower-yes").value;
-  let lowerNo = document.getElementById("lower-no").value;
-  let symbolsYes = document.getElementById("symbols-yes").value;
-  let symbolsNo = document.getElementById("symbols-no").value;
-  let errorMsg = document.querySelector("#errorMsg");
+
+// const form = document.getElementById("password-form");
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault(); // prevents form from autosubmitting
+
+//   let characters = document.getElementById("characters").value;
+//   let numbersYes = document.getElementById("numbers-yes").value;
+//   let numbersNo = document.getElementById("numbers-no").value;
+//   let upperYes = document.getElementById("upper-yes").value;
+//   let upperNo = document.getElementById("upper-no").value;
+//   let lowerYes = document.getElementById("lower-yes").value;
+//   let lowerNo = document.getElementById("lower-no").value;
+//   let symbolsYes = document.getElementById("symbols-yes").value;
+//   let symbolsNo = document.getElementById("symbols-no").value;
+//   let errorMsg = document.querySelector("#errorMsg");
 
   // js for form:
   // numbers 6-225
@@ -26,4 +28,4 @@ form.addEventListener("submit", function (event) {
   // checkbox for upper-yes and upper-no
   // checkbox for lower Yes & lower no
   // checkbox for symbols yes & symbols
-}); 
+// }); 
